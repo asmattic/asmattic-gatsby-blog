6bd1ca22e4c81d22f03e64861c352df1d48fb8c3
Matt Oldfield <mattoldfieldweb@gmail.com>
2021-08-15 18:17:31 -0400

    updated resume, sidebar links, avatar

--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -21,9 +21,9 @@ const DomContent = () => (
     <div className={`${style.name} centerAlign`}>
       <div className={`${style.boxName} centerAlign`}>
         <h2>
-          Rolwin
+          Matthew
           {' '}
-          <span>Reevan</span>
+          <span>Oldfield</span>
         </h2>
       </div>
       <div className={`${style.badge} ${style.badgeGray}`}>Software Engineer</div>
@@ -39,19 +39,19 @@ const DomContent = () => (
             <FeatherIcon size="19" icon="calendar" />
             {' '}
           </span>
-&nbsp; &nbsp; May 9,1995
+&nbsp; &nbsp; March 2, 1992
         </li>
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="map-pin" /></span>
           {' '}
-&nbsp; &nbsp; Bangalore, India
+&nbsp; &nbsp; Tampa, Florida
         </li>
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="mail" /></span>
           {' '}
 &nbsp; &nbsp;
           <a
-            href="mailto:&#114;&#111;&#108;&#119;&#105;&#110;&#109;&#111;&#110;&#116;&#101;&#105;&#114;&#111;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
+            href="mailto:&#109;&#097;&#116;&#116;&#111;&#108;&#100;&#102;&#105;&#101;&#108;&#100;&#119;&#101;&#098;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
             target="_top"
           >
             <span className={style.emailHider}>@</span>
